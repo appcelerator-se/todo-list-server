194c75e1a64d3b365e89f8301d0be74a3136f665
Bert Grantges <bgrantges@appcelerator.com>
2015-11-20 16:31:51 -0600

    first!

---
+++ conf/appc.arrowdb.development.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for development
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "development",
+      "key": "Gn2AZ680wNDhL3PmIapKJIFQx2Ew2pkz",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user_dev",
+      "password": "r6MmYE3I1INQSYtJuW"
+    }
+  }
+};
---
+++ conf/appc.arrowdb.production.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for production
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "production",
+      "key": "sf9c68NdNnQ9ExBDZoeLcdvK5PZaa5GE",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user",
+      "password": "pjkpVtyU18ld1zqfcg"
+    }
+  }
+};
